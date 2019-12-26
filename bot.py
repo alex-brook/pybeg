4e04999fc65508dd1d57cc9b4964d808c0b8bc1f
NayanShrees <nayanYshrees@gmail.com>
2019-12-26 01:36:29 +0000

    testing secret message

--- bot.py
+++ bot.py
@@ -26,14 +26,16 @@ async def on_ready():
 
 def is_police_kill_message(message):
     id = discord.utils.get(client.get_all_members(), name=CURRENT_USER.split('#')[0], discriminator=CURRENT_USER.split('#')[1]).id
-    POLICE_KILL_MESSAGE = '@' + str(id) + ' The police are here, and they\'re after you! Type'
+    POLICE_KILL_MESSAGE = '<@' + str(id) + '> The police are here, and they\'re after you! Type'
     if POLICE_KILL_MESSAGE in message.content:
         secret_message = re.search('`.*`', message.content)
         print(secret_message)
 
 @client.event
 async def on_message(message):
-    is_police_kill_message(message)
+    if 'The police are here' in message.content:
+        print(message.content)
+        is_police_kill_message(message)
 #    if str(message.author) == TARGET_USER:
 #        embed_list = message.embeds
 #        for embed in embed_list:
