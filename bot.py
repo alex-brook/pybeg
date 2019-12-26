09414449cb32a0028b4c39af1d4db0d465a6a63c
NayanShrees <nayanYshrees@gmail.com>
2019-12-26 00:32:31 +0000

    change secret message to use ID

--- bot.py
+++ bot.py
@@ -11,7 +11,9 @@ GUILD = os.getenv('DISCORD_GUILD')
 TARGET_USER = os.getenv('TARGET_USER')
 CURRENT_USER = os.getenv('CURRENT_USER')
 
-POLICE_KILL_MESSAGE = f'@{CURRENT_USER} The police are here, and they\'re after you! Type'
+CURRENT_DISCRIMINATOR = CURRENT_USER.split('#')[1]
+ID = discord.utils.get(client.get_all_members(), name=CURRENT_USER, discriminator=CURRENT_DISCRIMINATOR).id
+POLICE_KILL_MESSAGE = f'@{ID} The police are here, and they\'re after you! Type'
 
 client = discord.Client()
 
@@ -27,7 +29,7 @@ async def on_ready():
     )
 
 def is_police_kill_message(message):
-    if POLICE_KILL_MESSAGE in message.content:
+        if POLICE_KILL_MESSAGE in message.content:
         secret_message = re.search('`.*`', message.content)
         print(secret_message)
 
